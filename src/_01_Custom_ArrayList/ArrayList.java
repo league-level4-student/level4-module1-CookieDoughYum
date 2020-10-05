8857bc9d1784c242d0b4947efbbbf47afa5c4f71
root <root@06aee389e54e>
2020-10-05 23:57:33 +0000

    Working on Custom Array List (insert method).

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -4,19 +4,41 @@ package _01_Custom_ArrayList;
 
 public class ArrayList <T>{
 	
+	T[] arr; 
+	
 	public ArrayList() {
+		arr = (T[]) new Object[0];
 	}
 	
 	public T get(int loc) throws IndexOutOfBoundsException {
 		
-		return null;
+		return arr[loc];
 	}
 	
 	public void add(T val) {
-		
+		T[] add=(T[]) new Object[arr.length+1];
+		for(int i=0; i<arr.length; i++) {
+			add[i]=arr[i];
+		}
+		add[add.length-1]=val;
+		arr=add;
 	}
 	
-	public void insert(int loc, T val) throws IndexOutOfBoundsException {
+	public void insert(int loc, T val) throws IndexOutOfBoundsException
+	{
+		T[] insert=(T[]) new Object[arr.length+1];
+		for(int i=0; i<insert.length; i++) {
+			if(i<loc) {
+					insert[i]=arr[i];
+			}
+			else if(i==loc) {
+				insert[i]=val;
+			}
+			else if(i>loc) {
+				insert[i]-1;
+			}
+			
+		}
 		
 	}
 	
